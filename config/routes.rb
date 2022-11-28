7b770f199b3ecb0065cd33e88ab3fa6e50c10c01
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-11-28 17:41:34 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-  get 'applications/index'
-  get 'applications/show'
-  get 'applications/new'
-  get 'applications/create'
-  get 'jobs/index'
-  get 'jobs/show'
-  get 'jobs/new'
-  get 'jobs/create'
-  get 'jobs/edit'
-  get 'jobs/destroy'
   devise_for :users
   root to: "pages#home"
 
   resources :jobs do
     resources :applications
   end
+  resources :pages
 
 end
