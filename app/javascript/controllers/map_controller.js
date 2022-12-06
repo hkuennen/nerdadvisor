a244d7cb5f6d012f742610c9cf07eb146f2f8f98
Luthfi Pratomo <lapratomo24@yahoo.com>
2022-12-06 10:40:02 +0100

    map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: 'mapbox://styles/adityaluthfi24/clbbzy8m7000h15r4wrt014v8',
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
