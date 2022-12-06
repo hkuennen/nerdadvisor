12810762fa4eea9c47188a8435558f46647ce2be
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-12-06 10:53:05 +0100

    user and job migration added

--- db/migrate/20221206094343_add_columns_to_user.rb
+++ db/migrate/20221206094343_add_columns_to_user.rb
@@ -1,5 +1,11 @@
 class AddColumnsToUser < ActiveRecord::Migration[7.0]
   def change
+    add_column :users, :firstName, :string
+    add_column :users, :lastName, :string
+    add_column :users, :city, :string
+    add_column :users, :category, :string
+    add_column :users, :skills, :text, array: true, default: []
+    add_column :users, :role, :string
     add_column :users, :xp, :integer
     add_column :users, :linkedin, :string
     add_column :users, :github, :string
