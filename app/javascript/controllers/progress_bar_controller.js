226148ba47cb2a62f55fa165f61162d113dba2a8
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-12-07 11:15:32 +0100

    progress-bar functionality added

--- app/javascript/controllers/progress_bar_controller.js
+++ app/javascript/controllers/progress_bar_controller.js
@@ -2,7 +2,48 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="progress-bar"
 export default class extends Controller {
+  static targets = [ "xp" ];
+  xp = 100;
+
   connect() {
-    console.log("Porgress Bar connected");
+    const anim = this.xpTargets[0].getAnimations()[0].effect
+
+    anim.setKeyframes(
+      [{
+        backgroundAttachment: "initial",
+        backgroundClip: "initial",
+        backgroundColor: "rgb(16, 246, 183)",
+        backgroundImage: "initial",
+        backgroundOrigin: "initial",
+        backgroundPositionX: "initial",
+        backgroundPositionY: "initial",
+        backgroundRepeatX: "initial",
+        backgroundRepeatY: "initial",
+        backgroundSize: "initial",
+        composite: "auto",
+        computedOffset: 0,
+        easing: "ease",
+        offset: 0,
+        width: "0%"
+      }, {
+        backgroundAttachment: "initial",
+        backgroundClip: "initial",
+        backgroundColor: "rgb(210, 23, 217)",
+        backgroundImage: "initial",
+        backgroundOrigin: "initial",
+        backgroundPositionX: "initial",
+        backgroundPositionY: "initial",
+        backgroundRepeatX: "initial",
+        backgroundRepeatY: "initial",
+        backgroundSize: "initial",
+        composite: "auto",
+        computedOffset: 1,
+        easing: "ease",
+        offset: 1,
+        width: `${this.xp}%`
+      }]
+    );
+
+    console.log(anim.getKeyframes());
   }
 }
