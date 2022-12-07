5a2977e10c6db65729710d9f48da225f2f4396a7
Hinnerk Kuennen <hinnerk.kuennen@gmail.com>
2022-12-07 16:24:46 +0100

    filter fixed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,8 +4,10 @@ class JobsController < ApplicationController
   def index
     # @jobs = Job.page(params[:page]).per(7)
     @jobs = Job.all
+
     @jobs = @jobs.where("title ILIKE :title", title: "%#{params[:title]}%") if params[:title].present?
     @jobs = @jobs.where("city ILIKE :city", city: "%#{params[:city]}%") if params[:city].present?
+
     if params[:ft] || params[:pt] || params[:fl] || params[:is] || params[:ws]
       @filtered_jobs_type = []
       @filtered_jobs_type << @jobs.where(_type: params[:ft]) if params[:ft]
@@ -13,18 +15,32 @@ class JobsController < ApplicationController
       @filtered_jobs_type << @jobs.where(_type: params[:fl]) if params[:fl]
       @filtered_jobs_type << @jobs.where(_type: params[:is]) if params[:is]
       @filtered_jobs_type << @jobs.where(_type: params[:ws]) if params[:ws]
-      @jobs = @filtered_jobs_type.flatten
+      arr = @filtered_jobs_type.flatten
+      @jobs = Job.where(id: arr.map(&:id)) 
     end
-    # if params[:tn] || params[:sm] || params[:md] || params[:lg] || params[:xl] || params[:xxl]
-    #   @filtered_jobs_company_size = []
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:tn]) if params[:tn]
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:sm]) if params[:sm]
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:md]) if params[:md]
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:lg]) if params[:lg]
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:xl]) if params[:xl]
-    #   @filtered_jobs_company_size << @jobs.where(companySize: params[:xxl]) if params[:xxl]
-    #   @jobs = @filtered_jobs_company_size.flatten
-    # end
+
+    @jobs = @jobs.where(category: params[:skills]) if (params[:skills] !== "Choose...")
+
+    if params[:tn] || params[:sm] || params[:md] || params[:lg] || params[:xl] || params[:xxl]
+      @filtered_jobs_company_size = []
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:tn]) if params[:tn]
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:sm]) if params[:sm]
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:md]) if params[:md]
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:lg]) if params[:lg]
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:xl]) if params[:xl]
+      @filtered_jobs_company_size << @jobs.where(companySize: params[:xxl]) if params[:xxl]
+      arr = @filtered_jobs_company_size.flatten
+      @jobs = Job.where(id: arr.map(&:id))
+    end
+
+    if params[:os] || params[:rm] || params[:hy]
+      @filtered_jobs_on_site = []
+      @filtered_jobs_on_site << @jobs.where(onsite: params[:os]) if params[:os]
+      @filtered_jobs_on_site << @jobs.where(onsite: params[:rm]) if params[:rm]
+      @filtered_jobs_on_site << @jobs.where(onsite: params[:hy]) if params[:hy]
+      arr = @filtered_jobs_on_site.flatten
+      @jobs = Job.where(id: arr.map(&:id))
+    end
 
   end
 
